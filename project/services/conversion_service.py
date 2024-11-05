4fb64be4e6718fa200fd30cd22778b2374976d9d
oGaabs_ <gabriel04.ok@gmail.com>
2024-11-05 18:49:40 -0300

    refac: add constants for ffmpeg args and avoid erros on os.remove

--- project/services/conversion_service.py
+++ project/services/conversion_service.py
@@ -5,27 +5,57 @@ import concurrent.futures
 
 static_ffmpeg.add_paths(weak=True)
 
+variable_bit_rate = "-q:a 0"
+overwrite_output = "-y"
+input_file = "-i"
+exclude_video_streams = "-vn"
+include_audio_streams = "-map a"
+
 class ConversionService:
     def __init__(self, max_workers=4):
         self.max_workers = max(max_workers, os.cpu_count())
+        
+    def build_ffmpeg_command(self, input_filename, output_filename):
+        command = [
+            "ffmpeg",
+            overwrite_output,
+            input_file, input_filename, 
+            exclude_video_streams,
+            include_audio_streams,
+            variable_bit_rate,
+            output_filename
+        ]
+        return command
 
-    def _convert_mp4_to_mp3(self, file_path):
+    def _convert_mp4_to_mp3(self, input_path):
         try:
-            output_file = os.path.splitext(file_path)[0] + ".mp3"
-            subprocess.run(["ffmpeg", "-y", "-i",  file_path, "-vn", "-map", "a", output_file], shell=True)
-            os.remove(file_path)
-            return output_file
+            output_path = os.path.splitext(input_path)[0] + ".mp3"
+            ffmpeg_command = self.build_ffmpeg_command(input_path, output_path)
+
+            subprocess.run(ffmpeg_command, shell=True, check=True)
+            
+            if os.path.exists(output_path):
+                os.remove(input_path)
+            else:
+                print(f"Output file not created for {input_path}.")
+                return None
+            
+            return output_path
         except Exception as e:
-            print(f"Error converting {file_path} to MP3: {e}")
+            print(f"Error converting {input_path} to MP3: {e}")
             return None
 
     def convert_mp4_to_mp3(self, video_files, progress_callback=None):
         try:
             with concurrent.futures.ProcessPoolExecutor(max_workers=self.max_workers) as executor:
                 futures = [executor.submit(self._convert_mp4_to_mp3, file_path) for file_path in video_files]
-                for future in futures:
+                results = []
+                for future in concurrent.futures.as_completed(futures):
                     result = future.result()
-                    if result and progress_callback:
+                    if result:
+                        results.append(result)
+                    if progress_callback:
                         progress_callback(1)
+                return results
         except Exception as e:
             print(f"Error converting files: {e}")
